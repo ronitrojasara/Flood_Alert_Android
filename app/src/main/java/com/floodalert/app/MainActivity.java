75e9348a846e0879cf56bebeae4d6d3341e5ca9d
ronitrojasara <ronitrojasara@gmail.com>
2024-05-31 19:31:32 +0530

    Notifications

--- app/src/main/java/com/floodalert/app/MainActivity.java
+++ app/src/main/java/com/floodalert/app/MainActivity.java
@@ -1,14 +1,23 @@
 package com.floodalert.app;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.widget.ProgressBar;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -20,6 +29,8 @@ import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivity extends Activity {
+//    private static final String CHANNEL_ID = "my_channel";
+//    int notification_id = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +41,18 @@ public class MainActivity extends Activity {
         DatabaseReference databaseReference = mDatabase.child("sensor");
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser != null){
+            requestNotificationPermission(this);
+            try{
+                Intent serviceIntent = new Intent(this, MyForegroundService.class);
+                startForegroundService(serviceIntent);
+            }catch (Exception e){
+                Toast.makeText(this, "Service not running", Toast.LENGTH_SHORT).show();
+            }
+
             setContentView(R.layout.activity_main);
+
+//            createNotificationChannel();
+
             ProgressBar progressBar = findViewById(R.id.circularSeekBar);
             ProgressBar progressBar2 = findViewById(R.id.circularSeekBar2);
 
@@ -64,19 +86,19 @@ public class MainActivity extends Activity {
                         temperature.setText(""+temperature_value+ "°C");
                     }
 
-                    Float waterLevel_value = snapshot.child("Level").getValue(Float.class);
-                    if (waterLevel_value!=null){
-                        waterLevel_value = 50 - waterLevel_value;
+                    Float waterLevel_value = 50 - snapshot.child("Level").getValue(Float.class);
                         if (waterLevel_value>=0){
                             progressBar3.setProgress(waterLevel_value.intValue());
                             waterLevel.setText(""+waterLevel_value+" ft");
                         }
-                    }
+
 
                     Integer A1_value = snapshot.child("TP1").getValue(Integer.class);
                     Integer A2_value = snapshot.child("TP2").getValue(Integer.class);
                     Integer B1_value = snapshot.child("TP3").getValue(Integer.class);
                     Integer B2_value = snapshot.child("TP4").getValue(Integer.class);
+
+
 
                     if (A1_value!=null && A2_value!=null && B1_value!=null && B2_value!=null){
                         if (A1_value==1){
@@ -107,11 +129,16 @@ public class MainActivity extends Activity {
                             B2.setBackgroundColor(Color.rgb(0,255,0));
                             B2.setTextColor(Color.rgb(0,0,0));
                         }else{
+
                             B2.setTextColor(Color.rgb(255,255,255));
                             B2.setBackgroundColor(Color.rgb(255,0,0));
                         }
 
+//                        if (!(A1_value==1 && A2_value==1 && B1_value==1 && B2_value==1)||waterLevel_value>40){
+//                            showNotification("Flood Alert", "Evacuate to higher ground now");
+//                        }
                     }
+
                 }
 
                 @Override
@@ -145,4 +172,66 @@ public class MainActivity extends Activity {
 
 
     }
+    public static void requestNotificationPermission(Context context) {
+        if (!NotificationManagerCompat.from(context).areNotificationsEnabled()) {
+            showPermissionDialog(context);
+        }
+    }
+
+    private static void showPermissionDialog(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Notification Permission Required");
+        builder.setMessage("This app requires notification permission. Please enable it in the app settings.");
+
+        builder.setPositiveButton("Go to Settings", (dialog, which) -> openAppSettings(context));
+
+        builder.setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss());
+
+        builder.show();
+    }
+
+    private static void openAppSettings(Context context) {
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", context.getPackageName(), null);
+        intent.setData(uri);
+        context.startActivity(intent);
+    }
+
+//    private void createNotificationChannel() {
+//        CharSequence name = "My Channel";
+//        String description = "Channel Description";
+//        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+//        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+//        channel.setDescription(description);
+//
+//        // Customize channel settings if needed
+//        channel.enableLights(true);
+//        channel.setLightColor(Color.RED);
+//        channel.enableVibration(true);
+//
+//        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+//        notificationManager.createNotificationChannel(channel);
+//    }
+//    private void showNotification(String title, String content) {
+//        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+//                .setSmallIcon(R.drawable.priority_high_24px)
+//                .setContentTitle(title)
+//                .setContentText(content)
+//                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+//
+//        // Optionally, you can set additional properties for the notification, such as actions, intent, etc.
+//
+//        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//        notificationManager.notify(++notification_id, builder.build());
+//    }
+
+//    @Override
+//    protected void onDestroy() {
+//        Intent serviceIntent = new Intent(this, MyForegroundService.class);
+//        startForegroundService(serviceIntent);
+//        super.onDestroy();
+//    }
+
+
 }
--- app/src/main/java/com/floodalert/app/MyForegroundService.java
+++ app/src/main/java/com/floodalert/app/MyForegroundService.java
@@ -1,4 +1,111 @@
 package com.floodalert.app;
 
-public class MyForegroundService {
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.IBinder;
+import android.util.Log;
+import androidx.core.app.NotificationCompat;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+public class MyForegroundService extends Service {
+
+    private static final String CHANNEL_ID = "ForegroundServiceChannel";
+    int notification_id = 2;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        createNotificationChannel();
+        createNotificationChannel0();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Your background logic goes here
+        // For simplicity, let's just show a notification
+        showNotification("Flood Alert", "We will notify you immediately if flooding is detected in your area.");
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("sensor");
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child("TP1").getValue(Integer.class)==0
+                        ||dataSnapshot.child("TP2").getValue(Integer.class)==0
+                        ||dataSnapshot.child("TP3").getValue(Integer.class)==0
+                        ||dataSnapshot.child("TP4").getValue(Integer.class)==0||dataSnapshot.child("Level").getValue(Integer.class)<10){
+                    showNotification0("Flood Alert", "Evacuate to higher ground now");
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w("error","cancelled");
+            }
+        };
+        mDatabase.addValueEventListener(postListener);
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void createNotificationChannel() {
+        NotificationChannel channel = new NotificationChannel(
+                CHANNEL_ID,
+                "Foreground Service Channel",
+                NotificationManager.IMPORTANCE_HIGH
+        );
+        NotificationManager manager = getSystemService(NotificationManager.class);
+        manager.createNotificationChannel(channel);
+    }
+
+    private void showNotification(String title, String content) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.baseline_shield_24)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        startForeground(1, builder.build());
+    }
+    private void showNotification0(String title, String content) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "my_channel")
+                .setSmallIcon(R.drawable.priority_high_24px)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        // Optionally, you can set additional properties for the notification, such as actions, intent, etc.
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(++notification_id, builder.build());
+    }
+    private void createNotificationChannel0() {
+        CharSequence name = "My Channel 2";
+        String description = "Channel Description";
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel("my_channel2", name, importance);
+        channel.setDescription(description);
+
+        // Customize channel settings if needed
+        channel.enableLights(true);
+        channel.setLightColor(Color.RED);
+        channel.enableVibration(true);
+
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+    }
 }
